dbafa2d6db07d15e9e3ee987a5e0f716898ea6db
dilini <dilinichamikasilva@gmail.com>
2025-03-17 18:50:22 +0530

    init the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.gdse71.serenitymentalhealththerapycentersystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.gdse71.serenitymentalhealththerapycentersystem to javafx.fxml;
+    exports lk.ijse.gdse71.serenitymentalhealththerapycentersystem;
+}
