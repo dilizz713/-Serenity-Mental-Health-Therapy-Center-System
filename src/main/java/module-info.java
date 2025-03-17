d56b80e147d0763dd6ae7c12a117f728d9456f75
dilini <dilinichamikasilva@gmail.com>
2025-03-17 18:56:44 +0530

    updated module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,14 @@ module lk.ijse.gdse71.serenitymentalhealththerapycentersystem {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires java.sql;
+    requires lombok;
+
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
+    requires java.naming;
+
     opens lk.ijse.gdse71.serenitymentalhealththerapycentersystem to javafx.fxml;
     exports lk.ijse.gdse71.serenitymentalhealththerapycentersystem;
+
 }
