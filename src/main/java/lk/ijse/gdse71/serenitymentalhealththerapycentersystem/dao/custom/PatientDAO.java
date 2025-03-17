258de6088babdba56aabcabaf010aa0cbf9cbf6e
dilini <dilinichamikasilva@gmail.com>
2025-03-17 19:18:31 +0530

    create daos

---
+++ src/main/java/lk/ijse/gdse71/serenitymentalhealththerapycentersystem/dao/custom/PatientDAO.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse71.serenitymentalhealththerapycentersystem.dao.custom;
+
+public interface PatientDAO {
+}
---
+++ src/main/java/lk/ijse/gdse71/serenitymentalhealththerapycentersystem/dao/custom/impl/PatientDAOImpl.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse71.serenitymentalhealththerapycentersystem.dao.custom.impl;
+
+public class PatientDAOImpl {
+}
